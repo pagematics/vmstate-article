9eee772ecb8a4dca2e678ee7ae4dc6450baefff0
alka-john94 <alka.john@pagematics.in>
2023-04-02 20:27:24 -0500

    Alka: Committing abstract factory

--- src/main/java/vmstate/designpattern/factory/case001/FactorySuperClass.java
+++ src/main/java/vmstate/designpattern/factory/case001/FactorySuperClass.java
@@ -6,9 +6,9 @@ public abstract class FactorySuperClass
 	public abstract String getObject2();
 	public abstract String getObject3();
 	
-	@Override
-	public String toString()
-	{
-		return "Object1= "+this.getObject1()+", Object2="+this.getObject2()+", Object3="+this.getObject3();
-	}
+//	@Override
+//	public String toString()
+//	{
+//		return "Object1= "+this.getObject1()+", Object2="+this.getObject2()+", Object3="+this.getObject3();
+//	}
 }
