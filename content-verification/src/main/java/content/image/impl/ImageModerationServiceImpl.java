3a8f73d36ae10b17262070c9c10f6b7f02414084
alka-john94 <alka.john@pagematics.in>
2023-04-26 15:39:56 -0500

    Alka: added logger

--- content-verification/src/main/java/content/image/impl/ImageModerationServiceImpl.java
+++ content-verification/src/main/java/content/image/impl/ImageModerationServiceImpl.java
@@ -2,6 +2,9 @@ package content.image.impl;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import com.microsoft.azure.cognitiveservices.vision.contentmoderator.ContentModeratorClient;
 import com.microsoft.azure.cognitiveservices.vision.contentmoderator.ContentModeratorManager;
@@ -11,15 +14,17 @@ import com.microsoft.azure.cognitiveservices.vision.contentmoderator.models.Eval
 import content.EvaluationData;
 import content.ImageModerationResponseHB;
 import content.image.ImageModerationService;
+import content.text.impl.TextModerationServiceImpl;
 
 @Service("imageModerationService")
 public class ImageModerationServiceImpl implements ImageModerationService
 {
 	private static final String SUBSCRIPTION_KEY = "bd65975c08a7d446f8a7130d76";
 	private static final String ENDPOINT = "https:/cognitiveservices.azure.com/";
+	private static final Logger	logger	= LoggerFactory.getLogger(TextModerationServiceImpl.class);
 
 	@Override
-	public ImageModerationResponseHB moderateImage(String imageurl, boolean imageReviewRequired) 
+	public ImageModerationResponseHB moderateImage(String imageurl, boolean checkImage) 
 	{
 		ImageModerationResponseHB imageModerationResponseHB = new ImageModerationResponseHB();
 		ContentModeratorClient client = ContentModeratorManager.authenticate(AzureRegionBaseUrl.fromString(ENDPOINT), SUBSCRIPTION_KEY);
@@ -29,12 +34,14 @@ public class ImageModerationServiceImpl implements ImageModerationService
         List<EvaluationData> resultsList = new ArrayList<EvaluationData>();
         EvaluationData imageData = new EvaluationData();
         imageData.ImageUrl = url.value();
+        logger.debug("The image url: "+imageurl+" is checking for adult and racy content");
         imageData.ImageModeration = client.imageModerations().evaluateUrlInput("application/json", url, new EvaluateUrlInputOptionalParameter().withCacheImage(true));
         resultsList.add(imageData);
         if ((imageData.ImageModeration.isImageAdultClassified()==true)||(imageData.ImageModeration.isImageRacyClassified()==true))
         {
         	imageModerationResponseHB.setImageReviewRequired(true);
         }
+        logger.debug("Image moderation result for the image: "+imageurl+ "is: "+"imageReviewRequired = "+imageModerationResponseHB.isImageReviewRequired());
         return imageModerationResponseHB;
 	}
 	 
--- content-verification/src/main/java/content/text/impl/TextModerationServiceImpl.java
+++ content-verification/src/main/java/content/text/impl/TextModerationServiceImpl.java
@@ -1,6 +1,8 @@
 package content.text.impl;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.microsoft.azure.cognitiveservices.vision.contentmoderator.ContentModeratorClient;
@@ -17,6 +19,7 @@ public class TextModerationServiceImpl implements TextModerationService
 {
 	private static final String SUBSCRIPTION_KEY = "bd65975c08974a7130d76";
 	private static final String ENDPOINT = "https:/cognitiveservices.azure.com/";
+	private static final Logger	logger	= LoggerFactory.getLogger(TextModerationServiceImpl.class);
 	
 	@Override
 	public TextModerationResponseHB moderateText(String comment, String language, boolean checkPII, boolean checkProfanity)
@@ -27,6 +30,7 @@ public class TextModerationServiceImpl implements TextModerationService
     	options.withLanguage(language);
     	options.withPII(checkPII);
     	options.withClassify(checkProfanity);
+    	logger.debug("The comment: "+comment+"is undergoing content moderation with parameters: PII = "+checkPII+", Profanity = "+checkProfanity);
 		ContentModeratorClient client = ContentModeratorManager.authenticate(AzureRegionBaseUrl.fromString(ENDPOINT), SUBSCRIPTION_KEY);
         textResults = client.textModerations().screenText("text/plain", comment.getBytes(), options);
         if (textResults.pII()!=null)
@@ -37,6 +41,7 @@ public class TextModerationServiceImpl implements TextModerationService
         {
         	textModerationResponseHB.setProfanityReviewRecommended(true);
         }
+        logger.debug("Text moderation result for the comment: "+comment+ "is: "+"profanityReviewRecommended = "+textModerationResponseHB.ispIIReviewRecommended()+", pIIReviewRecommended = "+textModerationResponseHB.ispIIReviewRecommended());
         return textModerationResponseHB;
         
 	}
