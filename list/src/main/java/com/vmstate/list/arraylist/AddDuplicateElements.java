def18df7ea48c029c2be363c8f217052888370a4
alka.john <alka.john@pagematics.in>
2023-05-05 09:51:29 -0500

    Alka: committing arraylist example

---
+++ list/src/main/java/com/vmstate/list/arraylist/AddDuplicateElements.java
@@ -0,0 +1,24 @@
+package com.vmstate.list.arraylist;
+
+import java.util.ArrayList;
+
+public class AddDuplicateElements 
+{
+	public static void main(String arg[])
+	{
+		 ArrayList<String> arrayList = new ArrayList<String>(); 
+
+		 // Adding elements to the list
+		 arrayList.add("Nila");
+		 arrayList.add("Kevin");
+		 arrayList.add("Anna");
+		 arrayList.add("Kevin");
+		    
+		 System.out.println("Students List: "+ arrayList); 
+		 System.out.println("Student at index1: "+ arrayList.get(1)); 
+		 System.out.println("Student at index2: "+ arrayList.get(3)); 
+		 
+		 
+	}
+
+}
